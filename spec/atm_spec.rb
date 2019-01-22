30d98976b9741d3c95751e0edd1c8df34d3e41fd
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-01-22 13:57:50 +0100

    adds account object to pass rspec testing

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -16,7 +16,7 @@ describe ATM do
     end
 
     it 'funds are reduced at withdrawal' do
-        subject.withdraw(50)
+        subject.withdraw(50, account)
         expect(subject.funds).to eq 950
     end
     

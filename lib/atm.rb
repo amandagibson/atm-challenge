33d0fe4074f32cdf72835c367f6e533d72063c19
Per Eriksson <per_hegg@hotmail.com>
2019-01-23 12:05:31 +0100

    Adds a method to check is pin is wrong

--- lib/atm.rb
+++ lib/atm.rb
@@ -12,6 +12,8 @@ class Atm
         { status: false, message: 'insufficient funds', date: Date.today }
       when insufficient_funds_in_atm?(amount)
           { status: false, message: 'insufficient funds in ATM', date: Date.today }
+      when incorrect_pin?(pin_code, account.pin_code)
+          { status: false, message: 'wrong pin', date: Date.today }
       else
         perform_transaction(amount, account)
       end
@@ -32,7 +34,13 @@ class Atm
 
   def insufficient_funds_in_atm?(amount)
     @funds < amount
-   end
+  end
+
+  def incorrect_pin?(pin_code, actual_pin)
+    pin_code != actual_pin
+  end
+
+  
   
 end
 

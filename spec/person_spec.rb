95e2c2c0962b0751671b0487dc3f2690dab28791
Per Eriksson <per_hegg@hotmail.com>
2019-01-24 19:51:18 +0100

    adds name 'Per' to Person.new

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 describe Person do
 
-    subject { described_class.new(name: 'Thomas') }
+    subject { described_class.new(name: 'Per') }
 
         it '1. is expected to have a :name on initialize' do
             expect(subject.name).not_to be nil

13193acb68665ffbb3e5a790eb4dd669c4ae5207
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-01-24 14:13:50 +0100

    Adds account owner

--- README.md
+++ README.md
@@ -11,11 +11,11 @@ The ATM needs to have funds - DONE
 
 
 As an ATM operator
-In order for the right person to make a withdrawal <br>
+In order for the right person to make a withdrawal
 I want each person to have an separate account
 
 As a ATM operator
-In order ensure that an Acccount holder can only withdraw funds <br> that he has balance for I want to allow a withdrawal only if there are sufficient <br> funds in the account
+In order ensure that an Acccount holder can only withdraw funds that he has balance for I want to allow a withdrawal only if there are sufficient funds in the account
 
 As a Customer              
 In order to keep my funds secure             
@@ -24,3 +24,7 @@ I want a secure Pin code & an expiry date on my card that allows only me access 
 As a Customer
 In order to manage my funds
 I need to have a personal account
+
+As a ATM operator      
+In order to keep track of our costumers     
+We want to assign each account to a separate person
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,7 +2,8 @@ require './lib/account.rb'
 require 'date'
 
 describe Account do
-    
+    let(:person) {instance_double('Person', name: 'Thomas')}
+    subject { described_class.new({owner: person}) }
 
     it 'check length of a number' do
         number = 1234
@@ -15,11 +16,6 @@ describe Account do
         expect(subject.exp_date).to eq expected_date
     end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-end
-=======
-=======
     it 'is expected to have :active status on initialize' do
         expect(subject.account_status).to eq :active
     end
@@ -29,9 +25,14 @@ end
         expect(subject.account_status).to eq :deactivated
       end
 
->>>>>>> 1d970cbf2c4ecf0b2d0630b6e552bf60513e3311
+      it 'is expected to have an owner' do
+        expect(subject.owner).to eq person
+      end
 
+      it 'is expected to raise error if no owner is set' do
+        expect { described_class.new }.to raise_error 'An Account owner is required'
+      end
 
 
 end
->>>>>>> c1bba880675072835d2f0496ef43f1912670bae1
+
